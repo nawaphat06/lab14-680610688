b5955f06b7d36ba364b34785c1d37007ce44bcee
nawaphat06 <tantan.nawaphat@gmail.com>
2026-09-12 15:02:13 +0700

    finised

--- src/components/UserRegisterCard.tsx
+++ src/components/UserRegisterCard.tsx
@@ -1,7 +1,46 @@
 import type { Registrant } from "../libs/Registrant";
 
-export default function UserRegisterCard() {
+//5
+interface UserRegisterCardProps {
+  registrant: Registrant;
+}
+
+const itemLabels: Record<string, string> = {
+  bottle: "Bottle 🍼",
+  shoes: "Shoes 👟",
+  cap: "Cap 🧢",
+};
+
+export default function UserRegisterCard({
+  registrant,
+}: UserRegisterCardProps) {
   // registrant.gender === "male"   -> "👨 Male"
   //registrant.gender === "female" -> "👩 Female"
-  return <div className="card p-3"></div>;
+  const genderText = registrant.gender === "male" ? "👨 Male" : "👩 Female";
+
+  return (
+    <div className="card p-3 my-2 shadow-sm text-start">
+      <div className="d-flex justify-content-between align-items-start">
+        <div>
+          <h5 className="mb-1 fw-bold">{registrant.fullName}</h5>
+          <div className="text-secondary small mb-2">
+            {registrant.plan} · {genderText}
+          </div>
+
+          <div className="d-flex gap-2 flex-wrap">
+            {registrant.extraItems?.map((itemId) => (
+              <span
+                key={itemId}
+                className="badge bg-light text-dark border fw-bold px-2 py-1"
+              >
+                {itemLabels[itemId]}
+              </span>
+            ))}
+          </div>
+        </div>
+
+        <div className="fs-5">{registrant.total.toLocaleString()} THB</div>
+      </div>
+    </div>
+  );
 }
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,11 +1,35 @@
 import UserRegisterCard from "../components/UserRegisterCard";
 import type { Registrant } from "../libs/Registrant";
+// 6
+import { useState, useEffect } from "react";
 
 export default function DashboardPage() {
+  const [registrants, setRegistrants] = useState<Registrant[]>([]);
+
+  useEffect(() => {
+    const savedData = localStorage.getItem("registrants");
+    if (savedData) {
+      setRegistrants(JSON.parse(savedData));
+    }
+  }, []);
+
   return (
     <div className="container mt-4">
       <h2>Dashboard</h2>
+
       {/* Conditional Rendering + Render Component */}
+      {registrants.length === 0 ? (
+        <div className="alert alert-warning text-center" role="alert">
+          ยังไม่มีผู้ลงทะเบียน
+        </div>
+      ) : (
+        <div className="mt-3">
+          <div className="my-3">ผู้ลงทะเบียนแล้ว ({registrants.length} คน)</div>
+          {registrants.map((item) => (
+            <UserRegisterCard key={item.id} registrant={item} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
